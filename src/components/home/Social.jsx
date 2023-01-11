02e614b8119d4412f0e0481063ea55e6022418b2
Wildan <wildannurrahman30@gmail.com>
2023-01-11 16:33:36 +0700

    uploading portfolio to cloud

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Social = () => {
+    return(
+        <div className="home__social">
+            <a href="https://www.instagram.com/wildnr_/" className="home__social-icon" >
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/wildan-nur-rahman-96040b239/" className="home__social-icon" >
+                <i className="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href="https://github.com/wildanrhmn" className="home__social-icon" >
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social;
